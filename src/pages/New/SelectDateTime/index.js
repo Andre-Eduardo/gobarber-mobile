84bfaec20895f78343729b7f687bad8f66cf308e
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-05-31 23:26:56 -0300

    add page selectProvider

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import {View} from 'react-native';
-import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 const SelectDateTime = () => {
-  return <Background />;
+  return <View />;
 };
 
 export default SelectDateTime;
--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-const SelectProvider = () => {
-  return <Background />;
+import {Container, ProviderList, Provider, Avatar, Name} from './styles';
+
+const SelectProvider = ({navigation}) => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProvider() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+    loadProvider();
+  }, []);
+
+  return (
+    <Background>
+      <Container>
+        <ProviderList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({item: provider}) => (
+            <Provider
+              onPress={() => navigation.navigate('SelectDateTime', {provider})}>
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatars/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
+    </Background>
+  );
 };
 
 export default SelectProvider;
