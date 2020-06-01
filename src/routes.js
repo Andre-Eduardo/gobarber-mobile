60c3d2e43de54d93113519c2b1c1899046f6b98b
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-05-31 22:22:13 -0300

    add headerleft in stacknavigation

--- src/routes.js
+++ src/routes.js
@@ -7,15 +7,65 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {TouchableOpacity, Button} from 'react-native';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+import {CommonActions} from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function NewAppointment({navigation}) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        gestureEnabled: false,
+        headerTitleAlign: 'center',
+        headerTransparent: true,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerLeftContainerStyle: {
+          marginLeft: 20,
+        },
+        headerTintColor: '#fff',
+      }}>
+      <Stack.Screen
+        name="SelectProvider"
+        component={SelectProvider}
+        options={{
+          title: 'Selecione o pestador',
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.dispatch(CommonActions.goBack());
+              }}>
+              <Icon name="chevron-left" size={20} color="#fff" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="SelectDateTime"
+        component={SelectDateTime}
+        options={{title: 'Main'}}
+      />
+      <Stack.Screen
+        name="Confirm"
+        component={Confirm}
+        options={{title: 'Main'}}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function Routes({isSigned}) {
   return (
@@ -39,6 +89,17 @@ export default function Routes({isSigned}) {
                 <Icon color={color} size={20} name="event" />
               ),
               tabBarLabel: 'Agendamentos',
+            }}
+          />
+          <Tab.Screen
+            name="New"
+            component={NewAppointment}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon color={color} size={20} name="add-circle-outline" />
+              ),
+              tabBarLabel: 'Agendar',
+              tabBarVisible: false,
             }}
           />
           <Tab.Screen
